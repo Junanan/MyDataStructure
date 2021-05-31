7ddc17bf9066156d8b6c72aef397b6827a3da652
yangjunnan <yangjunnan@corp.netease.com>
2021-05-31 10:51:16 +0800

    排序更改

--- 排序/src/排序/InsertSort.java
+++ 排序/src/排序/InsertSort.java
@@ -3,29 +3,28 @@ package 排序;
 import java.util.Arrays;
 
 public class InsertSort {
-	public static void main(String[] args) {
-		int [] arr = new int[] {5,6,3,4};
-		insersort(arr);
-		System.out.println(Arrays.toString(arr));
-	}
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 6, 3, 4};
+        System.out.println(insersort(arr));
+    }
 
-	private static void insersort(int[] arr) {
-      //遍历所有的数字
-		int j ;
-		for(int i=1;i<arr.length;i++) {
-			//如果当前数字比前一个数字小
-			if(arr[i]<arr[i-1]) {
-				//把当前遍历数字存起来
-				int temp = arr[i];
-				//遍历当前数字前面所有的数字
-				for(j=i-1;j>=0&&temp<arr[j]	;j--){
-					//把前一个数字赋给后一个数字
-					arr[j+1] = arr[j];
-				}
-				//把临时变量（外层for循环的当前元素）赋给不满足条件的后一个元素
-				arr[j+1] = temp;
+    // 插入排序：稳定排序，在接近有序的情况下，表现优异
+
+	public static int[] insersort(int[] nums) {
+		int len = nums.length;
+		// 循环不变量：将 nums[i] 插入到区间 [0, i) 使之成为有序数组
+		for (int i = 1; i < len; i++) {
+			// 先暂存这个元素，然后之前元素逐个后移，留出空位
+			int temp = nums[i];
+			int j = i;
+			// 注意边界 j > 0
+			while (j > 0 && nums[j - 1] > temp) {
+				nums[j] = nums[j - 1];
+				j--;
 			}
+			nums[j] = temp;
 		}
+		return nums;
 	}
 
 }
